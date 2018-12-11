5ecb5211830d39bcf4a72eeaa4d738c112157909
tomamic <tomamic@ce.unipr.it>
2018-12-11 14:18:46 +0100

    ball

---
+++ python/p3_ball.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+ARENA_W, ARENA_H = 320, 240
+BALL_W, BALL_H = 20, 20
+
+class Ball:
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
+        self._dx = 5
+        self._dy = 5
+
+    def move(self):
+        if not (0 <= self._x + self._dx <= ARENA_W - BALL_W):
+            self._dx = -self._dx
+        if not (0 <= self._y + self._dy <= ARENA_H - BALL_H):
+            self._dy = -self._dy
+
+        self._x += self._dx
+        self._y += self._dy
+
+    def rect(self) -> (int, int, int, int):
+        return self._x, self._y, BALL_W, BALL_H
+
+
+def main():
+    # Create two objects, instances of the Ball class
+    b1 = Ball(140, 180)
+    b2 = Ball(180, 140)
+
+    for i in range(25):
+        print("b1 @", b1.rect(), "b2 @", b2.rect())
+        b1.move()
+        b2.move()
+
+##main()  # call main to start the program
