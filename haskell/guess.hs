d5e35229bb1ae186a8189d53ce810184abab2577
tomamic <tomamic@ce.unipr.it>
2023-04-25 12:32:50 +0200

    purer

--- haskell/guess.hs
+++ haskell/guess.hs
@@ -1,18 +1,17 @@
 import System.Random
 
-process gen ("":ls) = []
-process gen ls =
+process :: [Int] -> [String] -> [String]
+process secrets guesses =
     "Which number (1-10) am I thinking of?":
-        answer:
-        process newGen (tail ls)
-        -- if correct then [] else (process newGen (tail ls))
-    where
-        (secret, newGen) = randomR (1,10) gen :: (Int, StdGen)
-        correct = read (head ls) == secret
-        answer = if correct
-            then "You are correct!"
-            else "Sorry, it was " ++ show secret
+        check secrets guesses
+
+check :: [Int] -> [String] -> [String]
+check _ ("":_) = []
+check (secret:secrets) (guess:guesses)
+    | guess == show secret = ["You are correct!"]
+    | otherwise = ("Sorry, it was " ++ show secret) : process secrets guesses
 
 main = do
     gen <- getStdGen
-    interact $ unlines . (process gen) . lines
+    let secrets = randomRs (1,10) gen
+    interact $ unlines . (process secrets) . lines
