5183e241910d5771c6d498738dedd73489a14786
tomamic <tomamic@ce.unipr.it>
2026-03-21 12:13:20 +0100

    w64

--- haskell/xorshift.hs
+++ haskell/xorshift.hs
@@ -1,32 +1,38 @@
 -- Marsaglia https://doi.org/10.18637/jss.v008.i14
 
-import Data.Word (Word32)
+import Data.Word (Word64)
 import Data.Bits (xor, shiftL, shiftR)
 import Data.Time.Clock.POSIX (getPOSIXTime)
+import GHC.Exts (sortWith)
 
-type Rng32 = Word32
+type Rng = Word64
+(%%) = mod
 
-xorshift32 :: Rng32 -> Rng32
-xorshift32 a = d where
+xorshift :: Rng -> Rng
+xorshift a = d where
   b = a `xor` (a `shiftL` 13)
-  c = b `xor` (b `shiftR` 17)
-  d = c `xor` (c `shiftL` 5)
+  c = b `xor` (b `shiftR` 7)
+  d = c `xor` (c `shiftL` 17)
 
-randint :: (Int, Int) -> Rng32 -> (Int, Rng32)
-randint (nmin, nmax) gen = (val, nxt) where
-    nxt = xorshift32 gen
-    val = nmin + (fromIntegral nxt) `mod` (nmax + 1 - nmin)
+constrain :: (Int, Int) -> Rng -> Int
+constrain (nmin, nmax) n = fromIntegral n %% (nmax+1-nmin) + nmin
 
-randints :: (Int, Int) -> Rng32 -> [Int]
-randints range gen =
-    val : randints range nxt
-    where (val, nxt) = randint range gen
+randint :: (Int, Int) -> Rng -> (Int, Rng)
+randint range gen = (constrain range gen, xorshift gen)
+
+randints :: (Int, Int) -> Rng -> [Int]
+randints range gen = map (constrain range) $ iterate xorshift gen
+
+shuffle :: [a] -> Rng -> ([a], Rng)
+shuffle vals gen = (shuffled, nxt) where
+    (rs, nxt:_) = splitAt (length vals) $ iterate xorshift gen
+    shuffled = map snd $ sortWith fst $ zip rs vals
 
 chunksOf n [] = []
 chunksOf n xs = a : chunksOf n b where
   (a, b) = splitAt n xs
 
-getRng32 :: IO Rng32
-getRng32 = do
+getRng :: IO Rng
+getRng = do
     now <- getPOSIXTime
-    return (round (now * 1000) :: Rng32)
+    return (round (now * 1000) :: Rng)
